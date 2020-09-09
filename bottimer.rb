3cdbb5b850b19b846dec1dff18c63785fcfef679
saku35 <>
2020-09-09 19:32:42 +0900

    made a class for timer

--- bottimer.rb
+++ bottimer.rb
@@ -1,98 +1,84 @@
 require 'discordrb'
 
-def setTime(total)
-  @total = total
-end
+class Ytimer
+  attr_accessor :stopFlg
+  attr_writer :totalTime
 
-def totalTime()
-  @total ||= 10
-end
+  def initialize
+    @totalTime = 10
+    @prev = 0
+    @stopFlg = true
+  end
 
-def setPrev(tm)
-  @prev = tm
-end
+  def countDown(event)
+    @stopFlg = false
+    event.respond "#{Time.at(@totalTime).utc.strftime('%M:%S')}"
+    start = Time.now
+    loop do
+        break if @stopFlg
 
-def prevTime()
-  @prev ||= 0
-end
+        past = Time.now - start
+        break if past > @totalTime
+        it = @totalTime - past
+        next if (! canDisplay(it))
+        t = Time.at(it).utc.strftime('%M:%S')
+        @prev = it
+        event.respond "#{t}"
+        sleep 0.05
+    end
+    @stopFlg = true
+  end
 
-def setStopFlg(enable)
-  @stop = enable
-end
+  def canDisplay(remain)
+    diff = @prev - remain
+    return false if (diff.to_i == 0) 
 
-def stopFlg()
-  @stop
-end
+    unit = calcUnit(remain)
+    rem = (remain % unit).to_i
+    return (rem == 0)
+  end
 
-bot = Discordrb::Bot.new token: ENV['DISCODE']
-def countDown(event)
-
-  event.respond "#{Time.at(totalTime).utc.strftime('%M:%S')}"
-
-  start = Time.now
-  loop do
-      break if stopFlg
-
-      past = Time.now - start
-      break if past > totalTime
-      it = totalTime - past
-      next if (! canDisplay(it))
-      t = Time.at(it).utc.strftime('%M:%S')
-      setPrev(it)
-      event.respond "#{t}"
-      sleep 0.05
+  def calcUnit(tm)
+    return 30 if (61..nil).include? tm.to_i
+    return 10 if (11..60).include? tm.to_i
+    return 2 if (nil..10).include? tm.to_i
   end
 end
 
-def canDisplay(remain)
-  diff = prevTime - remain
-  return false if (diff.to_i == 0) 
-
-  unit = calcUnit(remain)
-  rem = (remain % unit).to_i
-  return (rem == 0)
-end
-
-def calcUnit(tm)
-  return 30 if (61..nil).include? tm.to_i
-  return 10 if (11..60).include? tm.to_i
-  return 2 if (nil..10).include? tm.to_i
-end
-
+bot = Discordrb::Bot.new token: ENV['DISCODE']
+timer = Ytimer.new
+  
 bot.message(with_text: 'set') do |event|
-  setTime(15)
+  timer.totalTime = 15
   event.respond '15秒 Setしました'
 end
 
 bot.message(with_text: 'set5') do |event|
-  setTime(5*60)
+  timer.totalTime = 5*60
   event.respond '5分 Setしました'
 end
 
 bot.message(with_text: 'set30') do |event|
-  setTime(30*60)
+  timer.totalTime = 30*60
   event.respond '30分 Setしました'
 end
 
 bot.message(with_text: 'stop') do |event|
   event.respond 'やめー'
-  setStopFlg(true)
+  timer.stopFlg = true
 end
 
 bot.message(with_text: 'start') do |event|
-  return unless stopFlg
+  return unless timer.stopFlg
 
   event.respond 'Timer Start!'
   event.respond "#{Time.now}"
 
-  setStopFlg(false)
-  countDown(event)
-  setStopFlg(true)
- 
+  timer.countDown(event)
+
   event.respond 'Timer End 888888'
   event.respond "#{Time.now}"
 end
 
-setStopFlg(true)
 bot.run
 
